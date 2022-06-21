2f5f8ed14d6f78522ac7a1e3f4e1f571fc4339aa
xiaoyun.zhang <xiaoyun.zhang@funplus.com>
2022-06-21 11:55:24 +0800

    暂定

--- Assets/Scripts/Lua/main.lua
+++ Assets/Scripts/Lua/main.lua
@@ -4,8 +4,9 @@ function main.start()
     print('Start')
     ---@type UnityEngine.GameObject
     local go = CS.UnityEngine.GameObject.Find('Cube')
-    CS.UnityEngine.Debug.LogError(go.name)
-    CS.UnityEngine.GameObject.Find("")
+    ---@type UnityEngine.Renderer
+    local render = go:GetComponent(typeof(CS.UnityEngine.Renderer))
+    render.material:SetColor("_Color", CS.UnityEngine.Color.red)
 end
 
 return main
