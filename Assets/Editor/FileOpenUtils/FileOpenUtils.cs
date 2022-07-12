c96f72dfffe5cb6c5cd20dd788ffb798161588eb
xiaoyun.zhang <xiaoyun.zhang@funplus.com>
2022-07-12 16:44:47 +0800

    FileOpenUtils

---
+++ Assets/Editor/FileOpenUtils/FileOpenUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using NPOI.HSSF.UserModel;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class FileOpenUtils {
+    [OnOpenAsset(0)]
+    private static bool OpenAssetStep1(int instanceId, int line) {
+        var path = AssetDatabase.GetAssetPath(instanceId);
+        if (ConfigFileOpenUtils.VerifyConfigFile(path)) {
+            ConfigFileOpenUtils.StartOpenConfigFile(path);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/Editor/FileOpenUtils/StringUtil.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Text;
+using System;
+
+public static class StringUtil {
+    public static readonly string[] splitSeparator = {"|"};
+    private static StringBuilder stringBuilder = new StringBuilder();
+    private static object lockObject = new object();
+
+    public static string Concat(params object[] objects) {
+        if (objects == null) {
+            return string.Empty;
+        }
+
+        lock (lockObject) {
+            stringBuilder.Length = 0;
+            foreach (var item in objects) {
+                if (item != null) {
+                    stringBuilder.Append(item);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+
+    public static bool CustomEndsWith(this string a, string b) {
+        int ap = a.Length - 1;
+        int bp = b.Length - 1;
+
+        if (ap < bp) {
+            return false;
+        }
+        
+        while (ap >= 0 && bp >= 0 && a[ap] == b[bp]) {
+            ap--;
+            bp--;
+        }
+
+        return (bp < 0 && a.Length >= b.Length) || (ap < 0 && b.Length >= a.Length);
+    }
+
+    public static bool CustomStartsWith(this string a, string b) {
+        int aLen = a.Length;
+        int bLen = b.Length;
+
+        if (aLen < bLen) {
+            return false;
+        }
+
+        int ap = 0;
+        int bp = 0;
+
+
+        while (ap < aLen && bp < bLen && a[ap] == b[bp]) {
+            ap++;
+            bp++;
+        }
+
+        return (bp == bLen && aLen >= bLen) || (ap == aLen && bLen >= aLen);
+    }
+    public static string Substring(string input, int startIndex, int length) {
+        if (string.IsNullOrEmpty(input)) {
+            throw new NullReferenceException("空字符串无法执行这项操作");
+        }
+
+        return input.Substring(startIndex, length);
+    }
+
+    public static void RenameGameObject(GameObject gameObject, params object[] objects) {
+#if UNITY_EDITOR
+        gameObject.name = Concat(objects);
+#endif
+    }
+}
